4a7ec20d9f159d3c1e8ce9f44f0d1bbaeb15456a
Ambrose Chua <ambrose@chua.family>
2017-11-12 10:15:37 +0800

    Updated config with Events url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,5 +3,6 @@ const HOST = "10.185.0.199";
 const CLIENT_ID = "";
 const CLIENT_SECRET = "";
 const ACCOUNTS_URL = "http://" + HOST + ":10202";
+const EVENTS_URL = "http://" + HOST + ":10203";
 
 export default { HOST, CLIENT_ID, CLIENT_SECRET, ACCOUNTS_URL };
